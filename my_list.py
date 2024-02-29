b384ca5255231b28f84bbbaafef1b50ee4466bfb
unknown <edoziegodwin@gmail.com>
2024-02-29 00:27:08 -0800

    first commit

---
+++ my_list.py
@@ -0,0 +1,13 @@
+my_list = []
+       # appending element to my_list
+my_list.extend([10 , 20 ,30 , 40])
+    #    insert the value 15 at second position
+my_list.insert(1, 15)
+        # extend my_list with anothe list
+my_list.extend([50, 60, 70])
+    # remove the last element
+my_list.pop()
+    #   sort in ascending order
+my_list.sort()
+# print the index of the value 30
+print(my_list[3])
